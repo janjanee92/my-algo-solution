a88b1d3ec3aef9e363825562ec6b5db6f56506e8
rowanlee92 <watrv41@gmail.com>
2021-07-17 17:25:19 +0900

    Add: LinkedList

---
+++ src/main/java/com/janjanee/hw/LinkedList.java
@@ -0,0 +1,115 @@
+package com.janjanee.hw;
+
+import java.util.HashSet;
+import java.util.NoSuchElementException;
+import java.util.Set;
+
+/**
+ * Q1. 정렬되어 있지 않은 LinkedList 중복 원소 제거
+ * @author leejihan
+ */
+public class LinkedList<E> {
+
+    private Node<E> first;
+    private Node<E> last;
+    private int size = 0;
+
+    static class Node<E> {
+        E item;
+        Node<E> next;
+
+        public Node(E item, Node<E> next) {
+            this.item = item;
+            this.next = next;
+        }
+    }
+
+    /**
+     * 중첩 루프를 이용한 중복 제거
+     * O(n^2)
+     */
+    public void removeDuplicate() {
+
+        Node<E> current = first;
+        Node<E> previous = first;
+
+        while (current != null) {
+            Node<E> check = current.next;
+
+            while(check != null) {
+                if(current.item.equals(check.item)) {
+                    unlink(check, previous);
+                } else {
+                    previous = check;
+                }
+                check = check.next;
+            }
+
+            current = current.next;
+        }
+    }
+
+    /**
+     * Hash를 이용한 중복 제거
+     * O(n)
+     */
+    public void removeDuplicateWithHash() {
+        Set<E> set = new HashSet<>();
+
+        Node<E> current = first;
+        Node<E> previous = null;
+
+        while (current != null) {
+            E item = current.item;
+
+            if(set.contains(item)) {
+                unlink(current, previous);
+            } else {
+                set.add(item);
+                previous = current;
+            }
+            current = current.next;
+        }
+    }
+
+    public void add(E item) {
+        Node<E> l = last;
+        Node<E> newNode = new Node<>(item, null);
+        last = newNode;
+        if(l == null)
+            first = newNode;
+        else
+            l.next = newNode;
+        size++;
+    }
+
+    public E remove() {
+        Node<E> f = first;
+        if (f == null)
+            throw new NoSuchElementException();
+        return unlinkFirst();
+    }
+
+    private E unlinkFirst() {
+        E element = first.item;
+        Node<E> next = first.next;
+        first.item = null;
+        first.next = null;
+        first = next;
+        if (next == null) {
+            last = null;
+        }
+        size--;
+        return element;
+    }
+
+    private void unlink(Node<E> current, Node<E> previous) {
+        previous.next = current.next;
+        size--;
+    }
+
+    public int size() {
+        return size;
+    }
+
+}
---
+++ src/test/java/com/janjanee/hw/LinkedListTest.java
@@ -0,0 +1,70 @@
+package com.janjanee.hw;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedListTest {
+
+
+    @Test
+    @DisplayName("중첩루프 중복 제거 - Integer")
+    void case_1() {
+        LinkedList<Integer> list = getIntegerLinkedList();
+        list.removeDuplicate();
+        assertEquals(7, list.size());
+    }
+
+    @Test
+    @DisplayName("해시 중복 제거 - Integer")
+    void case_2() {
+        LinkedList<Integer> list = getIntegerLinkedList();
+        list.removeDuplicateWithHash();
+        assertEquals(7, list.size());
+    }
+
+    @Test
+    @DisplayName("중첩루프 중복 제거 - String")
+    void case_3() {
+        LinkedList<String> list = getStringLinkedList();
+        list.removeDuplicate();
+        assertEquals(5, list.size());
+    }
+
+    @Test
+    @DisplayName("해시 중복 제거 - String")
+    void case_4() {
+        LinkedList<String> list = getStringLinkedList();
+        list.removeDuplicateWithHash();
+        assertEquals(5, list.size());
+    }
+
+    private LinkedList<Integer> getIntegerLinkedList() {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(4);
+        list.add(2);
+        list.add(1);
+        list.add(3);
+        list.add(4);
+        list.add(6);
+        list.add(3);
+        list.add(5);
+        list.add(10);
+        list.add(1);
+        return list;
+    }
+
+    private LinkedList<String> getStringLinkedList() {
+        LinkedList<String> list = new LinkedList<>();
+        list.add("hello");
+        list.add("hi");
+        list.add("hello");
+        list.add("bye");
+        list.add("cool");
+        list.add("happy");
+        list.add("bye");
+        return list;
+    }
+
+}
